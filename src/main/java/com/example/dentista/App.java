d72c2a1b287637d5e210a7dbab971397e77b3994
patxi <delriovergara.pablo@gmail.com>
2023-04-26 11:36:37 +0200

    Controller fechaLabel e iconos

--- src/main/java/com/example/dentista/App.java
+++ src/main/java/com/example/dentista/App.java
@@ -1,21 +1,22 @@
 package com.example.dentista;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainwindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("mainwindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 620, 400);
         stage.setTitle("Clínica Dental Del Río");
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("static/img/logo_dent.jpg")));
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/dentista/controller/MainWindowController.java
+++ src/main/java/com/example/dentista/controller/MainWindowController.java
@@ -3,7 +3,20 @@ package com.example.dentista.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class MainWindowController {
     @FXML
     private Label fechaLabel;
+
+    @FXML
+    public void cargarFecha(){
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd-MMM-uuuu");
+        String text = date.format(formatters);
+        fechaLabel.setText(text);
+    }
 }
