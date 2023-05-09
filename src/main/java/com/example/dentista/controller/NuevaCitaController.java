10a79f1459a160281a02136a5eca938e28cf45a3
patxi <delriovergara.pablo@gmail.com>
2023-05-09 11:05:31 +0200

    Check hours done, new date works

--- src/main/java/com/example/dentista/controller/NuevaCitaController.java
+++ src/main/java/com/example/dentista/controller/NuevaCitaController.java
@@ -52,14 +52,32 @@ public class NuevaCitaController implements Initializable {
     @FXML
     public boolean comprobarDuracion() {
         boolean comprobado = false;
-
+        if (duracionField.getText().matches("[0-9]+") && duracionField.getText().length() <= 2) {
+            comprobado = true;
+        }
         return comprobado;
     }
 
     @FXML
     public boolean comprobarDisponibilidad() {
-        boolean disponible = false;
+        boolean disponible = true;
+        ArrayList<String> fechasHoras = CitaTable.sacarFechasHoras(fechaCitaDtPicker.getValue(), new DataBaseConnection().getConnection());
+        System.out.println(fechasHoras);
+        for (int i = 0; i < fechasHoras.size(); i = i + 2) {
+            int duracionfield1 = (Integer.parseInt(duracionField.getText()) + (Integer.parseInt(fechasHoras.get(i))));
+            int duracionfield2 = (Integer.parseInt(duracionField.getText()) + (Integer.parseInt(fechasHoras.get(i + 1))));
+            if (duracionfield2 >= 60) {
+                duracionfield1 = Integer.parseInt(String.valueOf(duracionfield1).substring(0, 2)) * 100 + 100;
+                duracionfield2 = duracionfield2 - 60;
+            }
 
+            System.out.println(duracionfield1 + " - " + duracionfield2);
+            if (Integer.parseInt(horaComboBox.getValue().toString().replace(":", "")) <= duracionfield1
+                    && Integer.parseInt(horaComboBox.getValue().toString().replace(":", "")) > duracionfield2) {
+                disponible = false;
+            }
+        }
+        System.out.println(disponible);
         return disponible;
     }
 
@@ -72,16 +90,22 @@ public class NuevaCitaController implements Initializable {
                         descripcionTextArea.getText(), horaComboBox.getValue().toString(),
                         Integer.parseInt(duracionField.getText()));
                 if (CitaTable.nuevaCita(cita, new DataBaseConnection().getConnection())) {
-                    Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+                    Alert a = new Alert(Alert.AlertType.INFORMATION);
                     a.setTitle("REGISTRADO");
                     a.setContentText("SE HA CONFIRMADO EL REGISTRO DE SU CITA");
                     a.show();
                 }
             } else {
-
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("ERROR");
+                a.setContentText("Solo se pueden introducir dos dígitos numéricos");
+                a.show();
             }
         } else {
-
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("ERROR");
+            a.setContentText("Ya existe una cita durante las fechas señaladas");
+            a.show();
         }
 
     }
--- src/main/java/com/example/dentista/database/CitaTable.java
+++ src/main/java/com/example/dentista/database/CitaTable.java
@@ -4,7 +4,10 @@ import com.example.dentista.model.Cita;
 import com.example.dentista.model.Cliente;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class CitaTable {
 
@@ -13,7 +16,7 @@ public class CitaTable {
         try {
             Statement st = con.createStatement();
             int updated = st.executeUpdate("INSERT INTO citas VALUES ('" + cita.getFechaCita()
-                    + "','" + cita.getDniCliente() + "','" + cita.getDescripcion() + "','" + cita.getHora()
+                    + "','" + cita.getDniCliente() + "','" + cita.getDescripcion() + "','" + cita.getHora().replace(":","")
                     + "','" + cita.getDuracionMin() + "')");
             if (updated == 1) {
                 registrado = true;
@@ -23,5 +26,21 @@ public class CitaTable {
 
         }
         return registrado;
+    }
+
+    public static ArrayList<String> sacarFechasHoras(LocalDate fecha, Connection con) {
+        ArrayList<String> fechashoras = new ArrayList<>();
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT hora,duracion_min FROM citas WHERE fecha_cita = '" + fecha + "'");
+            while(rs.next()){
+                fechashoras.add(rs.getString(1));
+                fechashoras.add(rs.getString(2));
+            }
+            con.close();
+        } catch (Exception e) {
+
+        }
+        return fechashoras;
     }
 }
